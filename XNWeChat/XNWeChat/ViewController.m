8953414eb03d2b96e64055c2f5852f5b68c72e8f
夏宁 <1120814158@qq.com>
2016-11-03 01:15:38 +0800

    项目准备工作 继承XMPP

---
+++ XNWeChat/XNWeChat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XNWeChat
+//
+//  Created by user on 16/11/3.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
